f81438a12691959572469dbdc378c61bbdf1b12c
maor_el <maor.elias.91@gmail.com>
2016-06-04 17:10:49 +0300

    small change

--- src/mpp/Ex5q6.java
+++ src/mpp/Ex5q6.java
@@ -1,15 +1,11 @@
 package mpp;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Stream;
+
 
 public class Ex5q6 {
 
